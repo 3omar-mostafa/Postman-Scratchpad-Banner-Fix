a90f50ffe2609f7635e3c612b89140ea40127ba0
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2021-08-14 23:46:12 +0200

    Refactored some code

--- fix.js
+++ fix.js
@@ -2,53 +2,15 @@
 
 let fs = require('fs');
 let esprima = require('esprima');
-let _ = require('lodash');
 let colors = require('chalk'); // print colored text into console
 let { args } = require('./parse_args');
+let { findNestedObject, replaceRangeInString, printDebug } = require("./utils");
 
-/**
- * Get Object containing query from searchObject
- * @param searchObject Large object containing many nested objects
- * @param query The object filter to match
- * @returns Matching Object of the query
- */
-function getObject(searchObject, query) {
-  if (_.isMatch(searchObject, query)) {
-    return searchObject;
-  }
-  if (searchObject instanceof Array) {
-    for (let element of searchObject) {
-      let result = getObject(element, query);
-      if (result)
-        return result;
-    }
-  } else {
-    for (let key in searchObject) {
-      if (searchObject[key] instanceof Object || searchObject[key] instanceof Array) {
-        let result = getObject(searchObject[key], query);
-        if (result)
-          return result;
-      }
-    }
-  }
-}
-
-
-function replaceRangeInString(str, start, end, toReplace) {
-  if (toReplace === null) {
-    return str;
-  }
-  return str.substring(0, start) + toReplace + str.substring(end);
-}
-
-function printDebug(oldString, newString) {
-  console.log(`${colors.blue("[DEBUG]")} Replaced '${colors.red(oldString)}' with '${colors.green(newString)}'`);
-}
 
 function injectCode(rawFileData, searchQuery, replaceString) {
   // We need to reparse the data after each manipulation because we have changed their position
   let parsedJS = esprima.parseScript(rawFileData, { range: true, tolerant: true });
-  let parsedObject = getObject(parsedJS, searchQuery);
+  let parsedObject = findNestedObject(parsedJS, searchQuery);
   let range = parsedObject.range;
 
   printDebug(rawFileData.substring(range[0], range[1]), replaceString);
---
+++ utils.js
@@ -0,0 +1,43 @@
+let _ = require('lodash');
+let colors = require('chalk'); // print colored text into console
+
+
+/**
+ * Get Object containing query from searchObject
+ * @param searchObject Large object containing many nested objects
+ * @param query The object filter to match
+ * @returns Matching Object of the query
+ */
+function findNestedObject(searchObject, query) {
+  if (_.isMatch(searchObject, query)) {
+    return searchObject;
+  }
+  if (searchObject instanceof Array) {
+    for (let element of searchObject) {
+      let result = findNestedObject(element, query);
+      if (result)
+        return result;
+    }
+  } else {
+    for (let key in searchObject) {
+      if (searchObject[key] instanceof Object || searchObject[key] instanceof Array) {
+        let result = findNestedObject(searchObject[key], query);
+        if (result)
+          return result;
+      }
+    }
+  }
+}
+
+function replaceRangeInString(str, start, end, toReplace) {
+  if (toReplace === null) {
+    return str;
+  }
+  return str.substring(0, start) + toReplace + str.substring(end);
+}
+
+function printDebug(oldString, newString) {
+  console.log(`${colors.blue("[DEBUG]")} Replaced '${colors.red(oldString)}' with '${colors.green(newString)}'`);
+}
+
+module.exports = { findNestedObject, replaceRangeInString, printDebug };
